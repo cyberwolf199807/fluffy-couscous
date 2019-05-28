5debb58d1d41c79cf5cf211faaead662df913839
Cyber_wolf <51040820+cyberwolf199807@users.noreply.github.com>
2019-05-28 11:55:26 -0500

    startup file to initiate the process

---
+++ gearup.sh
@@ -0,0 +1,21 @@
+#!/user/bin/bash
+count=1
+while [[ $count -ge 0 ]]
+do
+sleep 2s
+bash script.sh
+sleep 2s
+bash start.sh
+sleep 2s
+bash livestatus.sh
+sleep 2s
+bash livestatus1.sh
+sleep 2s
+bash livestatus3.sh
+sleep 2s
+rm 2897*
+rm livestatus71
+sleep 30s
+clear
+done
+
